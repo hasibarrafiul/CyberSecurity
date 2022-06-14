50b23da5fe73b658826cbfb7c4fb9739b5f9d1ac
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-06-15 04:30:27 +0600

    MTU

--- findMTU.py
+++ findMTU.py
@@ -4,7 +4,7 @@ class IN:
     IP_MTU_DISCOVER = 10
     IP_PMTUDISC_DO = 2
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-hostName = "103.49.169.150"
+hostName = "127.0.0.0"
 Port = 9999
 s.connect((hostName, Port))
 s.setsockopt(socket.IPPROTO_IP, IN.IP_MTU_DISCOVER, IN.IP_PMTUDISC_DO)
